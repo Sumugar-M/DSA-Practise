19a65296e98ce475b2711293feefe874f404291f
Sumugar-M <sumugar001@gmail.com>
2025-02-20 21:38:07 +0530

    day3

--- STRING/5string.h
+++ STRING/5string.h
@@ -1,6 +1,11 @@
 /*
 
+Find the First Non-Repeating Character
 
+Problem: Return the first character in the string that does not repeat.
+Example:
+🔹 Input: "swiss"
+🔹 Output: "w"
 
 */
 
@@ -9,6 +14,25 @@
 
 int main()
 {
+    char str[100];
+    scanf("%s", str);
+    int left_ptr = 0, rigth_ptr = strlen(str) - 1;
+
+    while(left_ptr < rigth_ptr) {
+        if(str[left_ptr] == str[rigth_ptr]) {
+            left_ptr++;
+            rigth_ptr = strlen(str) - 1;
+        } else {
+            if(rigth_ptr == left_ptr + 1) {
+                printf("%c", str[left_ptr]);
+                break;
+            } else {
+                rigth_ptr--;
+            }
+        }
+    }
+
     
+
     return 0;
 }
